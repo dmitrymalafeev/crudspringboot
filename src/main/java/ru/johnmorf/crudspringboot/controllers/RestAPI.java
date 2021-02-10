e2192e0acfb2f2dc9005e8f7122b682a118f2af6
JohnMorf <johnmorf1507@gmail.com>
2021-02-10 16:11:50 +0300

    fix update roles

--- src/main/java/ru/johnmorf/crudspringboot/controllers/RestAPI.java
+++ src/main/java/ru/johnmorf/crudspringboot/controllers/RestAPI.java
@@ -3,19 +3,24 @@ package ru.johnmorf.crudspringboot.controllers;
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import ru.johnmorf.crudspringboot.entities.Role;
 import ru.johnmorf.crudspringboot.entities.User;
+import ru.johnmorf.crudspringboot.repositories.RoleRepository;
 import ru.johnmorf.crudspringboot.repositories.UserRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
 @RequestMapping("admin/api/users")
 public class RestAPI {
 
+    private final RoleRepository roleRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public RestAPI(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public RestAPI(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
@@ -39,9 +44,11 @@ public class RestAPI {
 
     @PutMapping("{id}")
     public User update(@PathVariable("id") User userFromDb, @RequestBody User user) {
+        Collection<Role> oldRoles = userFromDb.getRoles();
         user.refreshRoles();
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         BeanUtils.copyProperties(user, userFromDb, "id");
+        oldRoles.forEach(roleRepository::delete);
 
         return userRepository.save(userFromDb);
     }
