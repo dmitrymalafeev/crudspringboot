94e61b065e0f4bb2549f17a7146199c0b1a1ab64
JohnMorf <johnmorf1507@gmail.com>
2021-02-22 14:16:36 +0300

    fix

---
+++ src/main/java/ru/johnmorf/crudspringboot/entities/Message.java
@@ -0,0 +1,21 @@
+//package ru.johnmorf.crudspringboot.entities;
+//
+//import lombok.Data;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Data
+//public class Message {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    private String from;
+//
+//    private String content;
+//
+//    @ManyToOne
+//    private User to;
+//}
